bc17e417bd2d967c9a7c648bb21e7814891dbe4b
Michael Kestler <michael.s.kestler@gmail.com>
2023-02-21 17:36:19 -0800

    deleted html from php index file

--- index.php
+++ index.php
@@ -7,17 +7,4 @@ if($_POST) {
 
 ?>    
 
-<h1>Get in touch!</h1>
-
-<form method = "post">
-<label for="email">Email address</label>
-<p><input type="email" name="email"></p>
-
-<label for="subject">Subject</label>
-<p><input type="text" name="subject"></p>
-
-<label for="body">What would you like to ask us?</label>
-<p><textarea name="body"></textarea></p>
-<p><input type="submit" name="submit" value="Submit"></p>
-</form>
 
