a987148a05547a3407eed08c7f2d16ef6eff8503
Michael Kestler <michael.s.kestler@gmail.com>
2023-02-23 20:07:20 -0800

    added jquery form validation to index.php. reorganized files

--- index.php
+++ index.php
@@ -62,29 +62,56 @@ else {//email address is good
 <body>
 <div class="container">
     <h1>Get in touch!</h1>
-    <p><span class="error">* required field</span></p>
+    <div id="error"><?php echo $error.$successMessage; ?></div>
     <form method="post" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>">
         <fieldset class ="form-group">
             <label for="email">Email address</label>
             <span class="error">* <?php echo $emailErr;?></span>
-            <input type="email" class="form-control" name="email">
+            <input type="email" class="form-control" id="email" name="email">
         </fieldset>
         <fieldset class="form-group">
             <label for="subject">Subject</label>
             <span class="error">* <?php echo $subjErr;?></span>
-            <input type="text" class="form-control" name="subject">
+            <input type="text" class="form-control" id="subject" name="subject">
         </fieldset>
         <fieldset class="form-group">
             <label for="body">What would you like to ask us?</label>
             <span class="error">* <?php echo $bodyErr;?></span>
-            <textarea class="form-control" name="body" rows="3"></textarea>
+            <textarea class="form-control" id="body" name="body" rows="3"></textarea>
         </fieldset>
         <input type="submit" class="btn btn-primary" name="submit" value="Submit">
     </form>
 </div>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js" integrity="sha384-w76AqPfDkMBDXo30jS1Sgez6pr3x5MlQ1ZAGC+nuZB+EYdgRZgiwxhTBTkF7CXvN" crossorigin="anonymous"></script>
 
-</body>
+    <script type = "text/javascript">
+        $("form").submit(function (e) {
+                let error = "";
+
+                if($('#email').val() === "") {
+                    error += "The email field is required.<br>"
+                }
+
+                if($('#subject').val() === "") {
+                    error += "The subject field is required.<br>"
+                }
+
+                if($("content").val() === "") {
+                    error += "The content field is required.<br>"
+                }
+                //test if there was an error or not
+
+                if(error != "") {
+                    $("#error").html('<div class="alert alert-danger"' + 
+                    'role="alert"><p><strong>There were error(s) in your form:</strong></p>' + error + '</div>');
+
+                    return false;
+                } else { //no errors
+                    return true;
+                } // end if-else
+            });
+        </script>
+    </body>
 </html>
 
 
