a65074321840206c25f2125e2c2ef14bdb376f6f
Michael Kestler <michael.s.kestler@gmail.com>
2023-02-23 20:15:53 -0800

    deleted php messages and styling from head

--- index.php
+++ index.php
@@ -53,9 +53,6 @@ else {//email address is good
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <style>
-        .error{color: #FF0000;}
-    </style>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous">
     <title>Contact Us</title>
 </head>
@@ -66,17 +63,14 @@ else {//email address is good
     <form method="post" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>">
         <fieldset class ="form-group">
             <label for="email">Email address</label>
-            <span class="error">* <?php echo $emailErr;?></span>
             <input type="email" class="form-control" id="email" name="email">
         </fieldset>
         <fieldset class="form-group">
             <label for="subject">Subject</label>
-            <span class="error">* <?php echo $subjErr;?></span>
             <input type="text" class="form-control" id="subject" name="subject">
         </fieldset>
         <fieldset class="form-group">
             <label for="body">What would you like to ask us?</label>
-            <span class="error">* <?php echo $bodyErr;?></span>
             <textarea class="form-control" id="body" name="body" rows="3"></textarea>
         </fieldset>
         <input type="submit" class="btn btn-primary" name="submit" value="Submit">
